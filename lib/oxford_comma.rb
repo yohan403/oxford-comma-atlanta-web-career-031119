b769f6d478ed2d9f1e362b86ee8ac95715d09f78
John Cho <johnycho16@gmail.com>
2019-03-02 10:01:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+# array = ["one", "two", "three"]
 
+def oxford_comma(array)
+  string = ""
+  max = array.size
+  if max == 1
+    string << array[0]
+  elsif max == 2
+      string << array[0] + " and " + array[1]
+  else
+    for i in 1..max-1 do
+      string << array[i-1]
+      string << ", "
+    end
+    string = string + "and " + array[-1]
+  end
+  return string
 end
