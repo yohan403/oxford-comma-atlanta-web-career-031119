25f5fe23637d3d701e6fdf4db6f7646c7856aa4b
John Cho <johnycho16@gmail.com>
2019-03-02 10:01:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   if max == 1
     string << array[0]
   elsif max == 2
-      string << array[0] + "and" + array[1]
+      string << array[0] + " and " + array[1]
   else
     for i in 1..max-1 do
       string << array[i-1]
